b08b9ad926c101aa3341407ddb9ad8031447eec6
EdsonBuzetti <126629330+EdsonBuzetti@users.noreply.github.com>
2023-05-09 20:24:20 -0300

    Update Pessoa.java

--- Pratica6_Ex02/src/Pessoa.java
+++ Pratica6_Ex02/src/Pessoa.java
@@ -1,11 +1,10 @@
-
-
 public class Pessoa {
     private String nome;
     private String email;
     private String telefone;
 
     public Pessoa(String nome, String email, String telefone) {
+        super();
         this.nome = nome;
         this.email = email;
         this.telefone = telefone;
@@ -34,4 +33,7 @@ public class Pessoa {
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
+
+
+
 }
